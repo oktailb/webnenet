d6f92cd6ae4a770df3ed5c2a04660462f53185bd
LECOQ Vincent <vincent.lecoq@gmail.com>
2024-07-02 19:30:01 +0900

    image loading still problem

--- brain.js
+++ brain.js
@@ -9,11 +9,11 @@ if (typeof brainjs == "undefined") {
       this.prediction = jsonConfig.prediction;
       this.neurons = {};
 
-	  this.initInputsFromJSON(jsonConfig, this.neurons)
-	  console.log(this.getSize());
+      this.initInputsFromJSON(jsonConfig, this.neurons);
+      //console.log(this.getSize());
       this.initNeuronsFromJSON(jsonConfig, this.neurons);
-	  console.log(this.getSize());
-	  
+      //console.log(this.getSize());
+
       this.layersDim = {};
       for (var index = 0; index <= this.neurons["OUTPUT"].getLayer(); index++)
         this.layersDim[index] = [0, 0];
@@ -22,13 +22,12 @@ if (typeof brainjs == "undefined") {
       }
     }
 
-	getSize() {
-		var size = 0;
-		for (var name in this.neurons)
-			size++;
-		return size;
-	}
-	
+    getSize() {
+      var size = 0;
+      for (var name in this.neurons) size++;
+      return size;
+    }
+
     mutate() {
       for (var name in this.neurons) this.neurons[name].mutate();
     }
@@ -56,87 +55,85 @@ if (typeof brainjs == "undefined") {
           layersDimCopy[this.neurons[name].layer][0]--
         );
     }
-sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-	  initInputsFromJSON(jsonConfig) {
-		jsonConfig.inputs.forEach((inputConfig, index) => {
-			if (inputConfig.type === "img")
-			{
-				var filename = inputConfig.data;
-				var prefix = inputConfig.neuronprefix;
-				var activationFunc = inputConfig.activation;
-				var img = new Image(); 
-			
-					console.log(img.complete);
-				img.onload = function () {
-				}
-				img.src = filename;
-					console.log(img.complete);
+    sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    initInputsFromJSON(jsonConfig) {
+      jsonConfig.inputs.forEach((inputConfig, index) => {
+        if (inputConfig.type === "img") {
+          var filename = inputConfig.data;
+          var prefix = inputConfig.neuronprefix;
+          var activationFunc = inputConfig.activation;
+          var img = new Image();
 
-				while(!img.complete)
-				{
-					console.log(img.complete);
-					this.sleep(10);
-				}
+          console.log(img.complete);
+          img.onload = function() {};
+          img.src = filename;
+          console.log(img.complete);
 
-				var canvas = document.getElementById("secondaryCanvas");
-				var ctx = canvas.getContext("2d");
-				var imgData = ctx.getImageData(0, 0, img.width, img.height); 
-				for (var x = 0 ; x < img.width ; x++) {
-					for (var y = 0 ; y < img.height ; y++) {
-						var name = prefix + "_" + x + "_" + y; 
-						var neuron = initNeuron(name, activationFunc);
-						this.neurons[name] = neuron;
-						neuron.addInput(imgData.data[4 * img.width * y + x * 4 + 0], 1.0);
-						neuron.addInput(imgData.data[4 * img.width * y + x * 4 + 1], 1.0);
-						neuron.addInput(imgData.data[4 * img.width * y + x * 4 + 2], 1.0);
-						neuron.addInput(imgData.data[4 * img.width * y + x * 4 + 3], 1.0);
-						neuron.bias = 0.0;
-						neuron.activationParams = inputConfig.activationParams;
-					}					
-				}
-			}
-		});
-	  }
+          /*          while (!img.complete) {
+            console.log(img.complete);
+            this.sleep(10);
+          }
 
-	  initNeuronsFromJSON(jsonConfig) {
-		jsonConfig.neurons.forEach((neuronConfig, index) => {
-		  const activationFunc = neuronConfig.activation;
-		  const name = neuronConfig.name;
-		  const neuron = new initNeuron(name, activationFunc);
-		  this.neurons[name] = neuron;
-		  neuron.x = neuronConfig.x;
-		  neuron.y = neuronConfig.y;
-		  if (neuronConfig.bias !== undefined) {
-			neuron.bias = neuronConfig.bias;
-		  }
+          var canvas = document.getElementById("secondaryCanvas");
+          var ctx = canvas.getContext("2d");
+          var imgData = ctx.getImageData(0, 0, img.width, img.height);
+          for (var x = 0; x < img.width; x++) {
+            for (var y = 0; y < img.height; y++) {
+              var name = prefix + "_" + x + "_" + y;
+              var neuron = initNeuron(name, activationFunc);
+              this.neurons[name] = neuron;
+              neuron.addInput(imgData.data[4 * img.width * y + x * 4 + 0], 1.0);
+              neuron.addInput(imgData.data[4 * img.width * y + x * 4 + 1], 1.0);
+              neuron.addInput(imgData.data[4 * img.width * y + x * 4 + 2], 1.0);
+              neuron.addInput(imgData.data[4 * img.width * y + x * 4 + 3], 1.0);
+              neuron.bias = 0.0;
+              neuron.activationParams = inputConfig.activationParams;
+            }
+          }
+          */
+        }
+      });
+    }
 
-		  if (neuronConfig.activationParams) {
-			neuron.activationParams = neuronConfig.activationParams;
-		  }
+    initNeuronsFromJSON(jsonConfig) {
+      jsonConfig.neurons.forEach((neuronConfig, index) => {
+        const activationFunc = neuronConfig.activation;
+        const name = neuronConfig.name;
+        const neuron = new initNeuron(name, activationFunc);
+        this.neurons[name] = neuron;
+        neuron.x = neuronConfig.x;
+        neuron.y = neuronConfig.y;
+        if (neuronConfig.bias !== undefined) {
+          neuron.bias = neuronConfig.bias;
+        }
 
-		  neuronConfig.inputs.forEach(input => {
-			let value;
-			if (typeof input.value === "string") {
-			  value = this.neurons[input.value];
-			} else if (typeof input.value === "function") {
-			  value = input.value;
-			} else {
-			  value = input.value;
-			}
-			neuron.addInput(value, input.factor);
-		  });
-		});
-	  }
+        if (neuronConfig.activationParams) {
+          neuron.activationParams = neuronConfig.activationParams;
+        }
 
-	  getLayerSize(neurons, layer) {
-		var size = 0;
-		for (var name in neurons) {
-		  if (neurons[name].layer == layer) size++;
-		}
-		return size;
-	  }
+        neuronConfig.inputs.forEach(input => {
+          let value;
+          if (typeof input.value === "string") {
+            value = this.neurons[input.value];
+          } else if (typeof input.value === "function") {
+            value = input.value;
+          } else {
+            value = input.value;
+          }
+          neuron.addInput(value, input.factor);
+        });
+      });
+    }
+
+    getLayerSize(neurons, layer) {
+      var size = 0;
+      for (var name in neurons) {
+        if (neurons[name].layer == layer) size++;
+      }
+      return size;
+    }
   }
 
   function initBrain(jsonConfig) {
